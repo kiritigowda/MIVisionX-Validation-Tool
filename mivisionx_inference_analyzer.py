7f4ea3ae3d2591379e58510d5749a49a4fe524a2
LakshmiKumar23 <kumar.lakshmi1994@gmail.com>
2019-09-30 17:18:25 -0700

    pahse 1

--- mivisionx_inference_analyzer.py
+++ mivisionx_inference_analyzer.py
@@ -30,6 +30,10 @@ colors =[
         (0,128,255),      # Top4
         (255,102,102),    # Top5
         ];
+raliList = ['original', 'warpAffine', 'contrast', 'rain', 
+			'brightness', 'colorTemp', 'exposure', 'vignette', 
+			'fog', 'snow', 'pixelate', 'SnPNoise', 
+			'gamma', 'rotate', 'jitter', 'blend']
 
 # Class to initialize Rali and call the augmentations 
 class DataLoader(RaliGraph):
@@ -181,10 +185,11 @@ def processClassificationOutput(inputImage, modelName, modelOutput, batchSize):
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--model_format',		type=str, required=True,	help='pre-trained model format, options:caffe/onnx/nnef [required]')
-	parser.add_argument('--model_name',			type=str, required=True,	help='model name           	 	    	   	 	          [required]')
-	parser.add_argument('--model',				type=str, required=True,	help='pre_trained model files   	    	    	      [required]')
-	parser.add_argument('--model_input_dims',	type=str, required=True,	help='n,c,h,w - batch size,channel,height,width           [required]')
-	parser.add_argument('--model_output_dims',	type=str, required=True,	help='n,c,h,w - batch size,channel,height,width           [required]')
+	parser.add_argument('--model_name',			type=str, required=True,	help='model name           	 	    	   	 	        [required]')
+	parser.add_argument('--model',				type=str, required=True,	help='pre_trained model files   	    	    	    [required]')
+	parser.add_argument('--batch_size',         type=str, required=True,	help='batch size for inference 							[required]')
+	parser.add_argument('--model_input_dims',	type=str, required=True,	help='c,h,w - batch size,channel,height,width           [required]')
+	parser.add_argument('--model_output_dims',	type=str, required=True,	help='c,h,w - batch size,channel,height,width           [required]')
 	parser.add_argument('--label',				type=str, required=True,	help='labels text file                       [required]')
 	parser.add_argument('--output_dir',			type=str, required=True,	help='output dir to store ADAT results       [required]')
 	parser.add_argument('--image_dir',			type=str, required=True,	help='image directory for analysis           [required]')
@@ -201,6 +206,7 @@ if __name__ == '__main__':
 	modelFormat = args.model_format
 	modelName = args.model_name
 	modelLocation = args.model
+	modelBatchSize = args.batch_size
 	modelInputDims = args.model_input_dims
 	modelOutputDims = args.model_output_dims
 	label = args.label
@@ -224,12 +230,14 @@ if __name__ == '__main__':
 
 	# set paths
 	modelCompilerPath = '/opt/rocm/mivisionx/model_compiler/python'
+	ADATPath= '/opt/rocm/mivisionx/toolkit/analysis_and_visualization/classification'
 	setupDir = '~/.mivisionx-validation-tool'
 	analyzerDir = os.path.expanduser(setupDir)
 	modelDir = analyzerDir+'/'+modelName+'_dir'
 	nnirDir = modelDir+'/nnir-files'
 	openvxDir = modelDir+'/openvx-files'
 	modelBuildDir = modelDir+'/build'
+	adatOutputDir = os.path.expanduser(outputDir)
 	inputImageDir = os.path.expanduser(imageDir)
 	trainedModel = os.path.expanduser(modelLocation)
 	labelText = os.path.expanduser(label)
@@ -240,11 +248,11 @@ if __name__ == '__main__':
 	finalImageResultsFile = modelDir+'/imageResultsFile.csv'
 
 	# get input & output dims
-	str_n_i ,str_c_i, str_h_i, str_w_i = modelInputDims.split(',')
-	n_i = int(str_n_i); c_i = int(str_c_i); h_i = int(str_h_i); w_i = int(str_w_i)
-	str_n_o, str_c_o, str_h_o, str_w_o = modelOutputDims.split(',')
-	n_o = int(str_n_o); c_o = int(str_c_o); h_o = int(str_h_o); w_o = int(str_w_o)
-
+	str_c_i, str_h_i, str_w_i = modelInputDims.split(',')
+	c_i = int(str_c_i); h_i = int(str_h_i); w_i = int(str_w_i)
+	str_c_o, str_h_o, str_w_o = modelOutputDims.split(',')
+	c_o = int(str_c_o); h_o = int(str_h_o); w_o = int(str_w_o)
+	modelBatchSizeInt = int(modelBatchSize)
 	# input pre-processing values
 	Ax=[0,0,0]
 	if(inputAdd != ''):
@@ -283,13 +291,13 @@ if __name__ == '__main__':
 	# Setup Text File for Demo
 	if (not os.path.isfile(analyzerDir + "/setupFile.txt")):
 		f = open(analyzerDir + "/setupFile.txt", "w")
-		f.write(modelFormat + ';' + modelName + ';' + modelLocation + ';' + modelInputDims + ';' + modelOutputDims + ';' + label + ';' + outputDir + ';' + imageDir + ';' + imageVal + ';' + hierarchy + ';' + str(Ax).strip('[]').replace(" ","") + ';' + str(Mx).strip('[]').replace(" ","") + ';' + fp16 + ';' + replaceModel)
+		f.write(modelFormat + ';' + modelName + ';' + modelLocation + ';' + modelBatchSize + ';' + modelInputDims + ';' + modelOutputDims + ';' + label + ';' + outputDir + ';' + imageDir + ';' + imageVal + ';' + hierarchy + ';' + str(Ax).strip('[]').replace(" ","") + ';' + str(Mx).strip('[]').replace(" ","") + ';' + fp16 + ';' + replaceModel)
 		f.close()
 	else:
 		count = len(open(analyzerDir + "/setupFile.txt").readlines())
 		if count < 10:
 			f = open(analyzerDir + "/setupFile.txt", "a")
-			f.write("\n" + modelFormat + ';' + modelName + ';' + modelLocation + ';' + modelInputDims + ';' + modelOutputDims + ';' + label + ';' + outputDir + ';' + imageDir + ';' + imageVal + ';' + hierarchy + ';' + str(Ax).strip('[]').replace(" ","") + ';' + str(Mx).strip('[]').replace(" ","") + ';' + fp16 + ';' + replaceModel)
+			f.write("\n" + modelFormat + ';' + modelName + ';' + modelLocation + ';' + modelBatchSize + ';' + modelInputDims + ';' + modelOutputDims + ';' + label + ';' + outputDir + ';' + imageDir + ';' + imageVal + ';' + hierarchy + ';' + str(Ax).strip('[]').replace(" ","") + ';' + str(Mx).strip('[]').replace(" ","") + ';' + fp16 + ';' + replaceModel)
 			f.close()
 		else:
 			with open(analyzerDir + "/setupFile.txt", "r") as fin:
@@ -301,7 +309,7 @@ if __name__ == '__main__':
 			with open(analyzerDir + "/setupFile.txt", "w") as fout:
 			    fout.writelines(data[1:])
 			with open(analyzerDir + "/setupFile.txt", "a") as fappend:
-				fappend.write("\n" + modelFormat + ';' + modelName + ';' + modelLocation + ';' + modelInputDims + ';' + modelOutputDims + ';' + label + ';' + outputDir + ';' + imageDir + ';' + imageVal + ';' + hierarchy + ';' + str(Ax).strip('[]').replace(" ","") + ';' + str(Mx).strip('[]').replace(" ","") + ';' + fp16 + ';' + replaceModel)
+				fappend.write("\n" + modelFormat + ';' + modelName + ';' + modelLocation + ';' + modelBatchSize + ';' + modelInputDims + ';' + modelOutputDims + ';' + label + ';' + outputDir + ';' + imageDir + ';' + imageVal + ';' + hierarchy + ';' + str(Ax).strip('[]').replace(" ","") + ';' + str(Mx).strip('[]').replace(" ","") + ';' + fp16 + ';' + replaceModel)
 				fappend.close()
 
 	# Compile Model and generate python .so files
@@ -309,14 +317,14 @@ if __name__ == '__main__':
 	if(os.path.exists(modelDir)):
 		# convert to NNIR
 		if(modelFormat == 'caffe'):
-			os.system('(cd '+modelDir+'; python '+modelCompilerPath+'/caffe_to_nnir.py '+trainedModel+' nnir-files --input-dims ' + modelInputDims + ')')
-			#os.system('(cd '+modelDir+'; python '+modelCompilerPath+'/nnir_update.py --batch-size 1 nnir-files nnir-files)')
+			os.system('(cd '+modelDir+'; python '+modelCompilerPath+'/caffe_to_nnir.py '+trainedModel+' nnir-files --input-dims 1,' + modelInputDims + ')')
+			os.system('(cd '+modelDir+'; python '+modelCompilerPath+'/nnir_update.py --batch-size ' + modelBatchSize + ' nnir-files nnir-files)')
 		elif(modelFormat == 'onnx'):
-			os.system('(cd '+modelDir+'; python '+modelCompilerPath+'/onnx_to_nnir.py '+trainedModel+' nnir-files --input_dims ' + modelInputDims + ')')
-			#os.system('(cd '+modelDir+'; python '+modelCompilerPath+'/nnir_update.py --batch-size 1 nnir-files nnir-files)')
+			os.system('(cd '+modelDir+'; python '+modelCompilerPath+'/onnx_to_nnir.py '+trainedModel+' nnir-files --input_dims 1,' + modelInputDims + ')')
+			os.system('(cd '+modelDir+'; python '+modelCompilerPath+'/nnir_update.py --batch-size ' + modelBatchSize + ' nnir-files nnir-files)')
 		elif(modelFormat == 'nnef'):
-			os.system('(cd '+modelDir+'; python '+modelCompilerPath+'/nnef_to_nnir.py '+trainedModel+' nnir-files )')
-			os.system('(cd '+modelDir+'; python '+modelCompilerPath+'/nnir_update.py --batch-size ' + str_n_i + ' nnir-files nnir-files)')
+			os.system('(cd '+modelDir+'; python '+modelCompilerPath+'/nnef_to_nnir.py '+trainedModel+' nnir-files --batch-size ' + modelBatchSize + ')')
+			#os.system('(cd '+modelDir+'; python '+modelCompilerPath+'/nnir_update.py --batch-size ' + modelBatchSize + ' nnir-files nnir-files)')
 		else:
 			print("ERROR: Neural Network Format Not supported, use caffe/onnx/nnef in arugment --model_format")
 			quit()
@@ -377,8 +385,8 @@ if __name__ == '__main__':
 	#setup for Rali
 	batchSize = 1
 	loader = DataLoader(inputImageDir, batchSize, ColorFormat.IMAGE_RGB24, Affinity.PROCESS_GPU, imageValidation)
-	imageIterator = ImageIterator(loader, reverse_channels=False)
-	print "Input shape", loader.input.shape()
+	imageIterator = ImageIterator(loader, reverse_channels=False,multiplier=Mx,offset=Ax)
+	#print "Input shape", loader.input.shape()
 	print ('Pipeline created ...')
 	print 'Image iterator created ... number of images', imageIterator.imageCount()
 	print 'Loader created ...num of images' , loader.getOutputImageCount()
@@ -414,7 +422,7 @@ if __name__ == '__main__':
 
 			# process output and display
 			start = time.time()
-			resultImage, topIndex, topProb = processClassificationOutput(frame, modelName, output, n_i)
+			resultImage, topIndex, topProb = processClassificationOutput(frame, modelName, output, modelBatchSizeInt)
 			#cv2.imshow(windowInput, frame)
 			#cv2.imshow(windowResult, resultImage)
 			end = time.time()
@@ -442,14 +450,14 @@ if __name__ == '__main__':
 			t_height = size[0][1]
 			headerX_start = int(250 -(t_width/2))
 			cv2.putText(progressImage,modelName,(headerX_start,t_height+(20+40)),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,0),2)
-			txt = 'Processed: '+str((n_i*x)+i+1)+' of '+str(totalImages*n_i)
+			txt = 'Processed: '+str((modelBatchSizeInt*x)+i+1)+' of '+str(totalImages*modelBatchSizeInt)
 			size = cv2.getTextSize(txt, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 2)
 			cv2.putText(progressImage,txt,(50,t_height+(60+40)),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,0),1)
 			# progress bar
 			cv2.rectangle(progressImage, (50,150), (450,180), (192,192,192), -1)
-			progressWidth = int(50+ (400*((n_i*x)+i+1))/(totalImages*n_i))
+			progressWidth = int(50+ (400*((modelBatchSizeInt*x)+i+1))/(totalImages*modelBatchSizeInt))
 			cv2.rectangle(progressImage, (50,150), (progressWidth,180), (255,204,153), -1)
-			percentage = int((((n_i*x)+i+1)/float(totalImages*n_i))*100)
+			percentage = int((((modelBatchSizeInt*x)+i+1)/float(totalImages*modelBatchSizeInt))*100)
 			pTxt = 'progress: '+str(percentage)+'%'
 			cv2.putText(progressImage,pTxt,(175,170),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,0),1)
 
@@ -477,30 +485,30 @@ if __name__ == '__main__':
 
 			# top 1 progress
 			cv2.rectangle(progressImage, (50,200), (450,230), (192,192,192), -1)
-			progressWidth = int(50 + ((400*correctTop1)/(totalImages*n_i)))
+			progressWidth = int(50 + ((400*correctTop1)/(totalImages*modelBatchSizeInt)))
 			cv2.rectangle(progressImage, (50,200), (progressWidth,230), (0,153,0), -1)
-			percentage = int((correctTop1/float(totalImages*n_i))*100)
+			percentage = int((correctTop1/float(totalImages*modelBatchSizeInt))*100)
 			pTxt = 'Top1: '+str(percentage)+'%'
 			cv2.putText(progressImage,pTxt,(195,220),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,0),1)
 			# top 5 progress
 			cv2.rectangle(progressImage, (50,250), (450,280), (192,192,192), -1)
-			progressWidth = int(50+ ((400*correctTop5)/(totalImages*n_i)))
+			progressWidth = int(50+ ((400*correctTop5)/(totalImages*modelBatchSizeInt)))
 			cv2.rectangle(progressImage, (50,250), (progressWidth,280), (0,255,0), -1)
-			percentage = int((correctTop5/float(totalImages*n_i))*100)
+			percentage = int((correctTop5/float(totalImages*modelBatchSizeInt))*100)
 			pTxt = 'Top5: '+str(percentage)+'%'
 			cv2.putText(progressImage,pTxt,(195,270),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,0),1)
 			# wrong progress
 			cv2.rectangle(progressImage, (50,300), (450,330), (192,192,192), -1)
-			progressWidth = int(50+ ((400*wrong)/(totalImages*n_i)))
+			progressWidth = int(50+ ((400*wrong)/(totalImages*modelBatchSizeInt)))
 			cv2.rectangle(progressImage, (50,300), (progressWidth,330), (0,0,255), -1)
-			percentage = int((wrong/float(totalImages*n_i))*100)
+			percentage = int((wrong/float(totalImages*modelBatchSizeInt))*100)
 			pTxt = 'Mismatch: '+str(percentage)+'%'
 			cv2.putText(progressImage,pTxt,(175,320),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,0),1)
 			# no ground truth progress
 			cv2.rectangle(progressImage, (50,350), (450,380), (192,192,192), -1)
-			progressWidth = int(50+ ((400*noGroundTruth)/(totalImages*n_i)))
+			progressWidth = int(50+ ((400*noGroundTruth)/(totalImages*modelBatchSizeInt)))
 			cv2.rectangle(progressImage, (50,350), (progressWidth,380), (0,255,255), -1)
-			percentage = int((noGroundTruth/float(totalImages*n_i))*100)
+			percentage = int((noGroundTruth/float(totalImages*modelBatchSizeInt))*100)
 			pTxt = 'Ground Truth unavailable: '+str(percentage)+'%'
 			cv2.putText(progressImage,pTxt,(125,370),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,0),1)
 			
@@ -508,6 +516,23 @@ if __name__ == '__main__':
 			end = time.time()
 			if(verbosePrint):
 				print '%30s' % 'Progress image created in ', str((end - start)*1000), 'ms'
+
+		#show RALI augmented images
+		#for img_ind in range(loader.getOutputImageCount()):
+			#cloned_image = image_batch
+			if augmentedResults[i] == 0:
+				cloned_image = image_batch[0:224, (i*224+i):(i*224+i+1)]
+				cv2.imshow('cloned_image', cv2.cvtColor(cloned_image, cv2.COLOR_RGB2BGR))
+				cv2.rectangle(image_batch[ 0:224, (i*224+i):(i*224+i+1)], ((i*224)+i,0),((i*224)+i+1,224), (255,0,0), 4, cv2.LINE_8, 0)
+				cv2.waitKey(0)
+				#cloned_image[(i*224+i):(i*224+i+1) , 0:224] = image_batch[(i*224+i):(i*224+i+1) , 0:224]
+			elif augmentedResults[i] > 0  and augmentedResults[i] < 6:
+				cloned_image = image_batch[(i*224+i):(i*224+i+1)]
+				cv2.imshow('cloned_image', cv2.cvtColor(cloned_image, cv2.COLOR_RGB2BGR))
+				cv2.rectangle(image_batch[(i*224+i):(i*224+i+1) , 0:224], ((i*224)+i,0),((i*224)+i+1,224), (0,255,0), 4, cv2.LINE_8, 0)
+				cv2.waitKey(0)
+				#cloned_image[(i*224+i):(i*224+i+1) , 0:224] = image_batch[(i*224+i):(i*224+i+1) , 0:224]
+			cv2.imshow('augmented_images', cv2.cvtColor(image_batch, cv2.COLOR_RGB2BGR))
 
 		# exit inference on ESC; pause/play on SPACEBAR; quit program on 'q'
 		key = cv2.waitKey(2)
@@ -523,10 +548,27 @@ if __name__ == '__main__':
 
 	print("\nSUCCESS: Images Inferenced with the Model\n")
 
+	# Create ADAT folder and file
+	print("\nADAT tool called to create the analysis toolkit\n")
+	if(not os.path.exists(adatOutputDir)):
+		os.system('mkdir ' + adatOutputDir)
+	
+	if(hierarchy == ''):
+		os.system('python '+ADATPath+'/generate-visualization.py -i '+finalImageResultsFile+
+		' -d '+inputImageDir+' -l '+labelText+' -m '+modelName+' -o '+adatOutputDir+' -f '+modelName+'-ADAT')
+	else:
+		os.system('python '+ADATPath+'/generate-visualization.py -i '+finalImageResultsFile+
+		' -d '+inputImageDir+' -l '+labelText+' -h '+hierarchyText+' -m '+modelName+' -o '+adatOutputDir+' -f '+modelName+'-ADAT')
+	print("\nSUCCESS: Image Analysis Toolkit Created\n")
+	print("Press ESC to exit or close progess window\n")
+
 	while True:
 		key = cv2.waitKey(2)
 		if key == 27:
 			cv2.destroyAllWindows()
-			break        
+			break   
 		if cv2.getWindowProperty(windowProgress,cv2.WND_PROP_VISIBLE) < 1:        
 			break
+
+	outputHTMLFile = os.path.expanduser(adatOutputDir+'/'+modelName+'-ADAT-toolKit/index.html')
+	os.system('firefox '+outputHTMLFile)
