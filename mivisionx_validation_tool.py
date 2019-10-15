837b611922b750cd8e7bbd7c8d6a53476dd79abe
LakshmiKumar23 <kumar.lakshmi1994@gmail.com>
2019-10-15 11:46:42 -0700

    done for bs 64 and bs16-mode1

--- mivisionx_validation_tool.py
+++ mivisionx_validation_tool.py
@@ -108,20 +108,20 @@ class DataLoader(RaliGraph):
 		self.setSeed(0)
 
 		#params for contrast
-		self.min_param = RaliIntParameter(min)
-		self.max_param = RaliIntParameter(max)
+		self.min_param = RaliIntParameter(0)
+		self.max_param = RaliIntParameter(255)
 		#param for brightness
-		self.alpha_param = RaliFloatParameter(alpha)
+		self.alpha_param = RaliFloatParameter(0.0)
 		#param for colorTemp		
-		self.adjustment_param = RaliFloatParameter(adjustment)
+		self.adjustment_param = RaliIntParameter(0)
 		#param for exposure
-		self.shift_param = RaliFloatParameter(shift)
+		self.shift_param = RaliFloatParameter(0.0)
 		#param for SnPNoise
-		self.sdev_param = RaliFloatParameter(sdev)
+		self.sdev_param = RaliFloatParameter(0.0)
 		#param for gamma
-		self.gamma_shift_param = RaliFloatParameter(gamma_shift)
+		self.gamma_shift_param = RaliFloatParameter(0.0)
 		#param for rotate
-		self.degree_param = RaliFloatParameter(degree)
+		self.degree_param = RaliFloatParameter(0.0)
 
 		if model_batch_size == 16:
 			if raliMode == 1:
@@ -130,22 +130,22 @@ class DataLoader(RaliGraph):
 		        
 				self.warped = self.warpAffine(self.input,True)
 
-				self.contrast_img = self.contrast(self.input,True)
+				self.contrast_img = self.contrast(self.input,True, self.min_param, self.max_param)
 				self.rain_img = self.rain(self.input, True)
 
-				self.bright_img = self.brightness(self.input,True)
-				self.temp_img = self.colorTemp(self.input, True)
+				self.bright_img = self.brightness(self.input,True, self.alpha_param)
+				self.temp_img = self.colorTemp(self.input, True, self.adjustment_param)
 
-				self.exposed_img = self.exposure(self.input, True)
+				self.exposed_img = self.exposure(self.input, True, self.shift_param)
 				self.vignette_img = self.vignette(self.input, True)
 				self.fog_img = self.fog(self.input, True)
 				self.snow_img = self.snow(self.input, True)
 
 				self.pixelate_img = self.pixelate(self.input, True)
-				self.snp_img = self.SnPNoise(self.input, True, 0.2)
-				self.gamma_img = self.gamma(self.input, True)
+				self.snp_img = self.SnPNoise(self.input, True, self.sdev_param)
+				self.gamma_img = self.gamma(self.input, True, self.gamma_shift_param)
 
-				self.rotate_img = self.rotate(self.input, True)
+				self.rotate_img = self.rotate(self.input, True, self.degree_param)
 				self.jitter_img = self.jitter(self.input, True)
 				
 				self.blend_img = self.blend(self.input, self.contrast_img, True)
@@ -255,56 +255,57 @@ class DataLoader(RaliGraph):
         
 		self.warped = self.warpAffine(input_image,True)
 
-		self.contrast_img = self.contrast(input_image,True)
+		self.contrast_img = self.contrast(input_image,True, self.min_param, self.max_param)
 		self.rain_img = self.rain(input_image, True)
 
-		self.bright_img = self.brightness(input_image,True)
-		self.temp_img = self.colorTemp(input_image, True)
+		self.bright_img = self.brightness(input_image,True, self.alpha_param)
+		self.temp_img = self.colorTemp(input_image, True, self.adjustment_param)
 
-		self.exposed_img = self.exposure(input_image, True)
+		self.exposed_img = self.exposure(input_image, True, self.shift_param)
 		self.vignette_img = self.vignette(input_image, True)
 		self.fog_img = self.fog(input_image, True)
 		self.snow_img = self.snow(input_image, True)
 
 		self.pixelate_img = self.pixelate(input_image, True)
-		self.snp_img = self.SnPNoise(input_image, True, 0.2)
-		self.gamma_img = self.gamma(input_image, True)
+		self.snp_img = self.SnPNoise(input_image, True, self.sdev_param)
+		self.gamma_img = self.gamma(input_image, True, self.gamma_shift_param)
 
-		self.rotate_img = self.rotate(input_image, True)
+		self.rotate_img = self.rotate(input_image, True, self.degree_param)
 		self.jitter_img = self.jitter(input_image, True)
 		
 		self.blend_img = self.blend(input_image, self.contrast_img, True)
 
-	def updateAugmentationParameterInt(self, augmentation):
+    def updateAugmentationParameter(self, augmentation):
 		#values for contrast
-		min = int(augmentation)
-		max = 150 + (100-int(augmentation))
-		min_param.update(min)
-		max_param.update(max)
+		min = int(augmentation*100)
+		max = 150 + int((1-augmentation)*100)
+		self.min_param.update(min)
+		self.max_param.update(max)
 
 		#values for brightness
 		alpha = augmentation*1.95
-		alpha_param.update(alpha, self.brightness)
+		self.alpha_param.update(alpha)
 
 		#values for colorTemp
-		adjustment = augmentation*0.99 if (augmentation % 2 == 0) else (-1*augmentation*0.99)
-		adjustment_param.update(adjustment, self.colorTemp)
+		adjustment = (augmentation*99) if ((augmentation*100) % 2 == 0) else (-1*augmentation*99)
+		adjustment = int(adjustment)
+		self.adjustment_param.update(adjustment)
 
 		#values for exposure
 		shift = augmentation*0.95
-		shift_param.update(shift, self.exposure)
+		self.shift_param.update(shift)
 
 		#values for SnPNoise
 		sdev = augmentation*0.7
-		sdev_param.update(sdev, self.SnPNoise)
+		self.sdev_param.update(sdev)
 
 		#values for gamma
 		gamma_shift = augmentation*5.0
-		gamma_shift_param.update(gamma_shift, self.gamma)
+		self.gamma_shift_param.update(gamma_shift)
 
 		#values for rotation
 		degree = augmentation * 180.0
-		degree_param.update(degree, self.rotate)
+		self.degree_param.update(degree)
 
 # AMD Neural Net python wrapper
 class AnnAPI:
@@ -628,6 +629,7 @@ if __name__ == '__main__':
 		rali_batch_size = 1
 		start_rali = time.time()
 		loader = DataLoader(inputImageDir, rali_batch_size, modelBatchSizeInt, ColorFormat.IMAGE_RGB24, Affinity.PROCESS_CPU, imageValidation, h_i, w_i, raliMode)
+		loader.updateAugmentationParameter(0.35)
 		imageIterator = ImageIterator(loader, reverse_channels=False,multiplier=Mx,offset=Ax)
 		raliNumberOfImages = imageIterator.imageCount()
 		end_rali = time.time()
