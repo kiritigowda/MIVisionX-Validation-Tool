a7e412b08c6b62debe901964ce2bd42b8f7d52bc
Hansel Yang <hanselyang123@gmail.com>
2019-11-12 15:48:10 -0800

    bug fix

--- inference_viewer.py
+++ inference_viewer.py
@@ -148,10 +148,6 @@ class InferenceViewer(QtGui.QMainWindow):
         self.receiver_thread.terminate()
 
     def paintEvent(self, event):
-<<<<<<< HEAD
-=======
-    
->>>>>>> upstream/develop
         self.showAugImage()
         self.showImage()
         self.displayFPS()
